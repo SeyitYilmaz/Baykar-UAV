a6964504cd806a60f8480d13958ba4f857ff0a1d
Seyit Yılmaz <30367777+SeyitYilmaz@users.noreply.github.com>
2023-10-04 00:36:57 +0300

    Calculating Forces

---
+++ Assets/Scripts/Plane.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+public class Plane : MonoBehaviour
+{
+    public Vector3 Velocity {  get;  set; }
+    public Vector3 LocalVelocity { get;  set; }
+    public Vector3 LocalAngularVelocity { get;  set; }
+    public Vector3 LocalGForce { get;  set; }
+
+    public float Throttle {  get;  set; }
+    public float AngleOfAttack { get;  set; }
+    public float AngleOfAttackYaw { get; set; }
+}
---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    private Rigidbody planeRigidbody;
+    Plane currentPlane;
+    Vector3 previousVelocity;
+    void Awake()
+    {
+        planeRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        currentPlane = GetComponent<Plane>();
+    }
+
+    void CalculatePlaneState(float deltaTime)
+    {
+        var inverseRotation = Quaternion.Inverse(planeRigidbody.rotation);
+        currentPlane.Velocity = planeRigidbody.velocity;
+        currentPlane.LocalVelocity = inverseRotation * currentPlane.Velocity;
+        currentPlane.LocalAngularVelocity = inverseRotation * planeRigidbody.angularVelocity;
+
+        CalculateAngleOfAttack();
+    }
+    void CalculateAngleOfAttack () 
+    {
+        if(currentPlane.LocalVelocity.sqrMagnitude < 0.1f)
+        {
+            currentPlane.AngleOfAttack = 0;
+            currentPlane.AngleOfAttackYaw = 0;
+            return;
+        }
+        currentPlane.AngleOfAttack = Mathf.Atan2(-currentPlane.LocalVelocity.y, currentPlane.LocalVelocity.z);
+        currentPlane.AngleOfAttackYaw = Mathf.Atan2(currentPlane.LocalVelocity.x, currentPlane.LocalVelocity.z);
+    }
+    void CalculateGForce(float deltaTime)
+    {
+        var inverseRotation = Quaternion.Inverse(planeRigidbody.rotation);
+        var acceleration = (currentPlane.Velocity - previousVelocity) / deltaTime;
+        currentPlane.LocalGForce = inverseRotation * acceleration;
+        previousVelocity = currentPlane.Velocity;
+    }
+}
